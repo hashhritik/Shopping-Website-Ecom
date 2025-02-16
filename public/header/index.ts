9214a8c9a13e4d4ed556bae92f6cb5ec61998913
Hritik Kumar Singh <85543420+hashhritik@users.noreply.github.com>
2025-02-16 14:57:57 +0530

    Add files via upload

---
+++ public/header/index.ts
@@ -0,0 +1,8 @@
+import image1 from "./image1.png";
+import image2 from "./image2.png";
+import image3 from "./imagestop.png";
+import image4 from "./imagebottom.png";
+import vector from "./Vector.png";
+import cart from "./cart.png";
+
+export { image1, image2, image3, image4, vector , cart};
