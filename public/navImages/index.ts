945f0da8bb1e56207cc2b43cac874f7abb59959b
Hritik Kumar Singh <85543420+hashhritik@users.noreply.github.com>
2025-02-16 15:06:46 +0530

    Add files via upload

---
+++ public/navImages/index.ts
@@ -0,0 +1,8 @@
+import bag from './Bag.png'
+import hamburger from './hamburger.png'
+import human from './Human.png'
+import search from './Search.png'
+import star from './Star.png'
+import userIcon from './userIcon.png'
+
+export {bag,hamburger,star,human,search,userIcon}
---
+++ public/newsletter/index.ts
@@ -0,0 +1,3 @@
+import image1 from '@/public/newsletter/image 2.png'
+import image2 from '@/public/newsletter/image 3.png'
+export {image1,image2}
